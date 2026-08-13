20070369b2de6212fe3033dd8f54e64c5c73591b
Konrad Michalik <km@move-elevator.de>
2026-08-13 08:57:25 +0200

    refactor: mark the recording facet test fixture as a readonly anonymous class

--- Tests/Unit/Token/ModalStateTokenBuilderTest.php
+++ Tests/Unit/Token/ModalStateTokenBuilderTest.php
@@ -147,11 +147,11 @@ final class ModalStateTokenBuilderTest extends TestCase
      */
     private function createRecordingFacet(string $identifier): FacetInterface
     {
-        return new class($identifier) implements FacetInterface {
+        return new readonly class($identifier) implements FacetInterface {
             /**
              * @param non-empty-string $identifier
              */
-            public function __construct(private readonly string $identifier) {}
+            public function __construct(private string $identifier) {}
 
             public function getIdentifier(): string
             {
