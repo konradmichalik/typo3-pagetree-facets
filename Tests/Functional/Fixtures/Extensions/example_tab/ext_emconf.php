549348423cee6c56ae5dbdf0321cf596a294ee92
Konrad Michalik <konrad.michalik@gmx.de>
2026-09-04 19:34:44 +0200

    fix: restore TYPO3 v13 constraint dropped by misconfigured rector rule

--- Tests/Functional/Fixtures/Extensions/example_tab/ext_emconf.php
+++ Tests/Functional/Fixtures/Extensions/example_tab/ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '8.3.0-8.5.99',
-            'typo3' => '14.0.0-14.99.99',
+            'typo3' => '13.4.0-14.99.99',
             'typo3_pagetree_facets' => '0.1.0-0.1.99',
         ],
         'conflicts' => [],
--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '8.3.0-8.5.99',
-            'typo3' => '14.0.0-14.99.99',
+            'typo3' => '13.4.0-14.99.99',
         ],
         'conflicts' => [],
         'suggests' => [
